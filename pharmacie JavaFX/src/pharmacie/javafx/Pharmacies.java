71641c1af3abf206cd32d4a9a650d8d967486ae4
sirinerbesprit <sirinerbesprit>
2021-04-11 20:58:18 +0100

    first commit

---
+++ pharmacie JavaFX/src/pharmacie/javafx/Pharmacies.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pharmacie.javafx;
+
+/**
+ *
+ * @author User
+ */
+public class Pharmacies {
+    
+  private int ID;
+  private String Nom;
+  private String Adresse;
+  private int Numtel;
+
+    public Pharmacies(int ID, String Nom, String Adresse, int Numtel) {
+        this.ID = ID;
+        this.Nom = Nom;
+        this.Adresse = Adresse;
+        this.Numtel = Numtel;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
+
+    public int getNumtel() {
+        return Numtel;
+    }
+  
+}
